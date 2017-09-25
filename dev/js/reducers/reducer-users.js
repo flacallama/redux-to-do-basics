1245464a487ebbc8d364aa988e7dc98e851ee6ed
Dylan Cunningham <gdylanc@gmail.com>
2017-09-25 11:34:13 -0700

    set up and took out all of buckys work

--- dev/js/reducers/reducer-users.js
+++ dev/js/reducers/reducer-users.js
@@ -1,33 +1,29 @@
-/*
- * The users reducer will always return an array of users no matter what
- * You need to return something, so if there are no users then just return an empty array
- * */
+
+
 
 export default function () {
-    return [
-        {
-            id: 1,
-            first: "Bucky",
-            last: "Roberts",
-            age: 71,
-            description: "Bucky is a React developer and YouTuber",
-            thumbnail: "http://i.imgur.com/7yUvePI.jpg"
-        },
-        {
-            id: 2,
-            first: "Joby",
-            last: "Wasilenko",
-            age: 27,
-            description: "Joby loves the Packers, cheese, and turtles.",
-            thumbnail: "http://i.imgur.com/52xRlm8.png"
-        },
-        {
-            id: 3,
-            first: "Madison",
-            last: "Williams",
-            age: 24,
-            description: "Madi likes her dog but it is really annoying.",
-            thumbnail: "http://i.imgur.com/4EMtxHB.png"
-        }
-    ]
+  return [
+    {
+      id: 1,
+      first: 'dylan',
+      last: 'cunningham',
+      age: 35,
+      description: 'tired of redux'
+    },
+    {
+      id: 2,
+      first: 'kyle',
+      last: 'barket',
+      age: 30,
+      description: 'thriving'
+    },
+    {
+      id: 3,
+      first: 'natalie',
+      last: 'despain',
+      age: 23,
+      description: 'killing it'
+    }
+
+  ]
 }
